5d3f8135ad28b3408cf4cc95281ee75d1353ab96
Erik Novak <novak.erik@gmail.com>
2019-09-30 15:32:06 +0200

    Store the flask template files

---
+++ microservice/routes/service.py
@@ -0,0 +1,57 @@
+# Embedding Route
+# Routes related to creatiung text embeddings
+
+import sys
+
+from flask import (
+    Blueprint, flash, g, redirect, request, session, url_for, jsonify, current_app as app
+)
+from werkzeug.exceptions import abort
+
+
+#################################################
+# Initialize the models
+#################################################
+
+# TODO: include the model initialization function
+
+#################################################
+# Setup the embeddings blueprint
+#################################################
+
+# TODO: provide an appropriate route name and prefix
+bp = Blueprint('service', __name__, url_prefix='/api/v1/service')
+
+
+@bp.route('/', methods=['GET'])
+def index():
+    # TODO: provide an appropriate output
+    return abort(501)
+
+# TODO: add an appropriate route name
+@bp.route('/second', methods=['GET', 'POST'])
+def second():
+    # TODO: assign the appropriate variables
+    variable = None
+    if request.method == 'GET':
+        # retrieve the correct query parameters
+        variable = request.args.get('variable', default='', type=str)
+    elif request.method == 'POST':
+        # retrieve the text posted to the route
+        variable = request.json['variable']
+    else:
+        # TODO: log exception
+        return abort(405)
+
+    try:
+        # TODO: add the main functionality with the model and variable
+        finish = True
+    except:
+        # TODO: log exception
+        # something went wrong with the request
+        return abort(400)
+    else:
+        # TODO: return the response
+        return jsonify({
+            "finish": finish
+        })
